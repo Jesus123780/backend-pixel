62ba656eaf32b330c35f4b6d1f19f900f8df854e
Jesus Juvinao <juvinaojesusd@gmail.com>
2023-10-25 20:08:46 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const geoip = require('geoip-lite');
+const fs = require('fs');
+
+const app = express();
+
+/**
+ * Middleware to get geo information based on IP address
+ * @param {express.Request} req
+ * @param {express.Response} res
+ * @param {express.NextFunction} next
+ */
+const getGeoInformation = (req, res, next) => {
+  const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  const geo = geoip.lookup(ip);
+  console.log("🚀 ~ file: index.js:16 ~ getGeoInformation ~ geo:", geo)
+
+  if (geo) {
+    req.geoData = {
+      lat: geo.ll[0],
+      lon: geo.ll[1]
+    };
+  } else {
+    req.geoData = null;
+  }
+
+  // Here, you can save the geoData into a database for further analysis
+
+  next();
+};
+
+app.use(getGeoInformation);
+
+app.get('/tracking_pixel', (req, res) => {
+  // Your tracking logic here
+  
+  const img = fs.readFileSync('./1x1_pixel.png');
+  res.writeHead(200, {'Content-Type': 'image/png' });
+  res.end(img, 'binary');
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
