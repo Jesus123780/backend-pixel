a8e9615fd4d76044c1ef0e148c1add1964209a04
Jesus Juvinao <juvinaojesusd@gmail.com>
2023-10-25 20:46:13 -0500

    feat: update all

--- src/app.js
+++ src/app.js
@@ -2,23 +2,26 @@ const express = require('express');
 const geoip = require('geoip-lite');
 const fs = require('fs');
 const { parseUserAgent } = require('./helpers');
-
+var ip = require("ip");
+console.log(ip.address())
 const app = express();
+require('dns').lookup(require('os').hostname(), function (err, add, fam) {
+  console.log('addr: ' + add);
+})
 
 /**
  * Middleware to get geo information based on IP address
  * @param {express.Request} req
  * @param {express.Response} res
  * @param {express.NextFunction} next
- */
+*/
 const getGeoInformation = (req, res, next) => {
-  const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
   const userAgent = req.headers['user-agent']
   const device = parseUserAgent(userAgent)
-  console.log("🚀 ~ file: app.js:17 ~ getGeoInformation ~ device:", device)  
-  const geo = geoip.lookup(ip);
-  console.log("🚀 ~ file: index.js:16 ~ getGeoInformation ~ geo:", geo)
-
+  console.log("🚀 ~ file: app.js:17 ~ getGeoInformation ~ device:", device)
+  const geo = geoip.lookup(ip.address());
+  console.log("🚀 ~ file: index.js:16 ~ getGeoInformation ~ geo:", ip.address())
+  
   if (geo) {
     req.geoData = {
       lat: geo.ll[0],
@@ -27,13 +30,18 @@ const getGeoInformation = (req, res, next) => {
   } else {
     req.geoData = null;
   }
-
+  
   // Here, you can save the geoData into a database for further analysis
-
+  
   next();
 };
+app.use(express.json()); // para parsear payloads de tipo JSON
 
+app.set('view engine', 'pug');
 app.use(getGeoInformation);
+app.get('/', (req, res) => {
+  res.render('index');
+});
 
 app.get('/tracking_pixel', (req, res) => {
   // Your tracking logic here
@@ -44,6 +52,16 @@ app.get('/tracking_pixel', (req, res) => {
 });
 
 const PORT = 3000;
+app.post('/save_location', (req, res) => {
+  const { lat, lon } = req.body;
+
+  // Aquí guardas los datos en tu base de datos
+  console.log(`Latitude: ${lat}, Longitude: ${lon}`);
+  
+  res.status(200).json({ message: 'Location saved' });
+});
+
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
